3fccf69ef58e914f6be41805c9aa9a0290b6655f
kushalkolar <kushalkolar@gmail.com>
2020-07-17 14:39:55 +0200

    works

--- aviewer/__main__.py
+++ aviewer/__main__.py
@@ -7,33 +7,47 @@ class Viewer(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
 
-        path = QtWidgets.QFileDialog(self, 'Choose file', '/', "(*.tiff *.tif)")
+        path = QtWidgets.QFileDialog.getOpenFileName(self, 'Choose file', '/', "(*.tiff *.tif)")
 
         if not path[0]:
             return
         path = path[0]
 
-        tif = TiffFile(path)
+        self.tif = TiffFile(path)
+
+        self.widget = QtWidgets.QWidget(parent=self)
+        self.vlayout = QtWidgets.QVBoxLayout(self.widget)
 
         iv = ImageView(parent=self)
-        iv.setImage(tif.series[0].asarray())
-        self.setCentralWidget(iv)
+        iv.setImage(self.tif.asarray(key=0))
+        self.vlayout.addWidget(iv)
 
-        slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        slider.setMaximum(len(tif.series))
-        slider.setMinimum(0)
-        slider.setSingleStep(1)
-        slider.setPageStep(10)
-        slider.show()
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, parent=self)
+        self.slider.setMaximum(len(self.tif.series) - 1)
+        self.slider.setMinimum(0)
+        self.slider.setSingleStep(1)
+        self.slider.setPageStep(10)
 
-        slider.valueChanged.connect(
+        self.spinbox = QtWidgets.QSpinBox(parent=self)
+        self.spinbox.setMaximum(len(self.tif.series) - 1)
+        self.spinbox.setMinimum(0)
+        self.spinbox.valueChanged.connect(self.slider.setValue)
+
+        self.vlayout.addWidget(self.spinbox)
+        self.vlayout.addWidget(self.slider)
+
+        self.slider.valueChanged.connect(
             lambda i: iv.setImage(
-                tif.series[i],
+                self.tif.asarray(key=i),
                 autoRange=False,
                 autoLevels=False,
                 autoHistogramRange=False
             )
         )
+
+        self.slider.valueChanged.connect(self.spinbox.setValue)
+
+        self.setCentralWidget(self.widget)
 
 
 if __name__ == '__main__':
